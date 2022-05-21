1a110a2861e0999c72743531add826d062de4f59
gabriel <gabimtme@gmail.com>
2022-05-21 17:38:27 +0200

    feat: layer agnostic gcode parsing

--- src/GCodeViewer/SceneElements/Camera.tsx
+++ src/GCodeViewer/SceneElements/Camera.tsx
@@ -4,16 +4,21 @@ import { PerspectiveCameraProps, useThree } from "@react-three/fiber";
 import { Vector3 } from "three";
 
 export interface CameraProps extends Partial<PerspectiveCameraProps> {
+    initialPosition?: { x: number, y: number, z: number}
 }
 
 const Camera: React.FC<CameraProps> = (
     {
+        initialPosition,
         ...otherProps
     }
 ) => {
     const {camera} = useThree()
     useEffect(() => {
         camera.up.applyAxisAngle(new Vector3(1, 0, 0), Math.PI/2)
+        if (initialPosition !== undefined) {
+            camera.position.set(initialPosition.x, initialPosition.y, initialPosition.z)
+        }
     }, [camera])
 
     return (
--- src/GCodeViewer/gcode/GCode.ts
+++ src/GCodeViewer/gcode/GCode.ts
@@ -2,10 +2,8 @@ import { BaseReader } from "./reader";
 import { GPoint } from "./types";
 import { GPointFromCmd, isMoveCmd } from "./parser";
 
-const MIN_POINTS_USEFUL_LAYER = 10
 const MAX_QUALITY_RES = 10
 const Z_BASE_CENTER_CALC_THRESHOLD = .4
-const Z_LAYER_HEIGHT_CALC_THRESHOLD = .4
 const Z_LIMITS_CALC_THRESHOLD = .4
 const BIG_NUM = 2**32
 const LINE_SPLIT = "\n"
@@ -13,6 +11,7 @@ const LINE_SPLIT = "\n"
 const sleep = (ms: number) => new Promise(res => setTimeout(res, ms))
 
 export interface GCodeParseProgress {
+    points: GPoint[]
     read: number
     baseCenter: {x: number, y: number}
     max: {x: number, y: number, z: number}
@@ -28,26 +27,25 @@ export interface GCodeOptions {
 
 export class GCode {
     private lastPoint: GPoint = {x: 0, y: 0, z: 0, e: 0, type: "travel"}
-    public readonly layers: Record<number, GPoint[]> = {}
+    public readonly points: GPoint[] = []
 
-    public layerHeight: number = 0
-    public firstLayerHeight: number = 0
     public baseCenter?: {x: number, y: number} = undefined
     public limits = {
         max: {x: 0, y: 0, z: 0},
         min: {x: BIG_NUM, y: BIG_NUM, z: 0}
     }
+    private opts: GCodeOptions
     public filament: number = 0
-    public readonly opts: GCodeOptions
-    private read: number = 0
-    private count: number = 0
+    private read = 0
+    private count = 0
+    private _abort = false
 
     private readonly omitCycle: boolean[] = []
     private readonly qualityRes: number
 
     constructor(opts: GCodeOptions = {}) {
         this.opts = opts
-        const quality = this.opts.quality || 1
+        const quality = opts.quality || 1
         if (quality > 1 || quality < 0) throw new Error("quality must be between 0 and 1")
         const q = quality > .5 ? (1-quality):quality
         this.qualityRes = Math.min(Math.ceil(1 / q), MAX_QUALITY_RES)
@@ -57,8 +55,13 @@ export class GCode {
         }
     }
 
-    private getProgress = (): GCodeParseProgress => {
+    abort = () => {
+        this._abort = true
+    }
+
+    private get getProgress(): GCodeParseProgress {
         return {
+            points: this.points,
             read: this.read,
             baseCenter: this.baseCenter ? {...this.baseCenter}:{x: 0, y: 0},
             max: {...this.limits.max},
@@ -70,7 +73,7 @@ export class GCode {
     public parse = async (reader: BaseReader) => {
         let prevCodeLine = ""
         this.read = 0
-        while (true) {
+        while (!this._abort) {
             const chunk = await reader.read()
             if (!chunk) break
             this.read += chunk.length
@@ -78,31 +81,21 @@ export class GCode {
             codeLines[0] = prevCodeLine + codeLines[0]
             prevCodeLine = codeLines.pop() || ""
             this.parseCodeLines(codeLines)
-            if (this.opts.onProgress) {
-                new Promise<void>((res) => {
-                    this.opts.onProgress(this.getProgress())
-                    res()
-                })
-            }
+            if (this.opts.onProgress) this.opts.onProgress(this.getProgress)
             await sleep(0)
         }
-        if (this.opts.onFinished) this.opts.onFinished(this.getProgress())
+        if (this.opts.onFinished) this.opts.onFinished(this.getProgress)
     }
 
     private updateBaseCenter() {
         let accX = 0
         let accY = 0
         let count = 0
-        const zs = Object.keys(this.layers).map(Number).sort()
-        for (const z of zs) {
-            if (z === 0) continue
-            for (const point of this.layers[z]) {
-                if (point.type !== "extrude") continue
-                accX += point.x
-                accY += point.y
-                count++
-            }
-            break
+        for (const point of this.points) {
+            if (point.type !== "extrude") continue
+            accX += point.x
+            accY += point.y
+            count++
         }
         if (count < 10) return
         this.baseCenter = {x: accX/count, y: accY/count}
@@ -116,34 +109,21 @@ export class GCode {
         if (move.e && move.e > this.filament) this.filament = move.e
     }
 
-    private updateLayerHeight() {
-        const zHeights = Object.keys(this.layers)
-            .map(Number)
-            .filter(z => this.layers[z].length > MIN_POINTS_USEFUL_LAYER)
-            .sort()
-        if (zHeights.length < 2) return
-        this.layerHeight = zHeights[1]-zHeights[0]
-        this.firstLayerHeight = zHeights[0]
-    }
-
     private parseCodeLines(codeLines: string[]) {
         for (const line of codeLines) {
             if (!isMoveCmd(line)) continue
             const point = GPointFromCmd(line, this.lastPoint)
-            let z = point.z
-            z = Math.round((point.z - point.z % (this.layerHeight || .2)) * 1000)/ 1000
+            
             if (point.z > Z_BASE_CENTER_CALC_THRESHOLD && !this.baseCenter) this.updateBaseCenter()
             if (point.z > Z_LIMITS_CALC_THRESHOLD && point.type === "extrude") this.updateLimits(point)
-            if (point.z > Z_LAYER_HEIGHT_CALC_THRESHOLD && !this.layerHeight) this.updateLayerHeight()
 
             this.lastPoint = point
 
-            if (!this.layers[z]) this.layers[z] = []
             if (point.type === "travel") {
-                this.layers[z].push(point)
+                this.points.push(point)
                 continue
             } else if (this.omitCycle[this.count % this.qualityRes]) {
-                this.layers[z].push(point)
+                this.points.push(point)
             }
             this.count++
         }
