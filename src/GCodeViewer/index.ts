b05dd3eec04626ad5996ea6b90a48575d6434317
gabriel <gabimtme@gmail.com>
2022-06-16 23:39:30 +0200

    feat: export all types

--- src/GCodeViewer/index.ts
+++ src/GCodeViewer/index.ts
@@ -1,3 +1,5 @@
 export { GCodeViewer } from "./GCodeViewer";
 export type { GCodeViewerProps } from "./GCodeViewer";
 export type { GCodeParseProgress } from "./gcode/GCode"
+export type { GPoint } from "./gcode/types"
+export type { FloorProps } from "./GCodeModel"
